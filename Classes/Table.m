763096429d4622b813286c8ee6a3d75327512f0f
Shahd Sumrain <shahdsumrain@outlook.com>
2024-10-25 11:57:01 +1100

    point cloud environment attempt

--- Classes/Table.m
+++ Classes/Table.m
@@ -1,13 +1,13 @@
 classdef Table < LoadObjectSTL
     properties
-        fileName = "table";
+        fileName = "table2";
         scaleX = 1.0;
         scaleY = 0.8;
         scaleZ = 1.0;
         transformation = SE3(0,0.15,0);
         faceColour = [0.6 0.3 0];
         edgeColour = [0.65 0.35 0.05];
-        identifier = "table";
+        identifier = "table2";
     end
     
     methods
