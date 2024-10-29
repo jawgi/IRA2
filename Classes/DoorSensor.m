16aab9b31caad6e20e0e4bfdbe9a86b0fb028b04
Shahd Sumrain <shahdsumrain@outlook.com>
2024-10-29 20:24:38 +1100

    Added array of drop points for stage 2 and 3 - rescaled environment to suit reach of robots better

--- Classes/DoorSensor.m
+++ Classes/DoorSensor.m
@@ -4,7 +4,7 @@ classdef DoorSensor < LoadObjectSTL
         scaleX = 1.0;
         scaleY = 2.0;
         scaleZ = 1.5;
-        transformation = SE3(1.915,-0.898,1.5);
+        transformation = SE3(1.73,-0.85,2);
         faceColour = [0, 0, 0];
         edgeColour = [0.1,0.1,0.1];
         identifier = "doorsensor";
